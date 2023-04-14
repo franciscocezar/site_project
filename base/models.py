9b708b8866916279bfd3ea8a60c3eba6fcac603e
Francisco Cezar <franciscocezar.jr@outlook.com>
2023-04-14 16:25:41 -0300

    Criando tabela Restaurante no banco de dados

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Restaurante(models.Model):
+    nome = models.CharField(max_length=100)
+    local = models.CharField(max_length=200)
+    cardapio = models.CharField(max_length=100)
+    horario = models.DateTimeField()
+    thumb = models.ImageField(upload_to='imagens')
+    descricao = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.nome
