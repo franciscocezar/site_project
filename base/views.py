f855694cf8d73d59b61412441eaf2f79af3cfe73
Francisco Cezar <franciscocezar.jr@outlook.com>
2023-04-14 22:14:19 -0300

    Adicionando diretorio templates

--- base/views.py
+++ base/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Bem-vindo ao Site de Recomendações de Campos do Jordão")
+    # return HttpResponse("Bem-vindo ao Site de Recomendações de Campos do Jordão")
+    return render(request, "index.html")
--- cjosite/urls.py
+++ cjosite/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('base/', include('base.urls')),
+    path('', include('base.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
